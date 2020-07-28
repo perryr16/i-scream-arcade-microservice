ceab7543f856da67a5a3b98ceb8bcb291cc16cf9
Mariana Cid <cidmariana5@gmail.com>
2020-07-27 18:26:26 -0600

    refactor

--- poros/game_results.rb
+++ poros/game_results.rb
@@ -22,11 +22,12 @@ class GameResults
       similars:     run_array(:get_games_by_id, :name, params[:similar_games]), # array of ids
       screenshots:  format_screenshots(params), # array of ids
       themes:       run_array(:get_game_themes, :name, params[:themes]), # array of ids
-      videos:       "https://www.youtube.com/watch?v=#{youtube_id(params[:videos][0])}" # good -> array of ids
+      videos:       "https://www.youtube.com/watch?v=#{youtube_id(params[:videos])}" # good -> array of ids
     }}
   end
 
   def run_array(method, key, array)
+    return nil if !array.is_a?(Array)
     array.map do |element|
       service.send(method,element)[0][key]
     end
@@ -37,7 +38,9 @@ class GameResults
   end
   
   def youtube_id(video_id)
-    service.get_game_videos(video_id)[0][:video_id]
+    # binding.pry
+    return nil if !video_id.is_a?(Array)
+    service.get_game_videos(video_id[0])[0][:video_id]
   end
 
   def keyword_id(word)
@@ -66,6 +69,30 @@ class GameResults
     
   def find_games_with_keyids(ids)
     games = service.get_games_by_keywords(ids)
+  end
+
+  def games_by_keyids(ids)
+    game_arrays = service.get_games_by_keyids(ids)
+    game_arrays.map do |game| 
+      # binding.pry
+    result = {data: {
+      age_ratings:  run_array(:get_age_ratings, :rating, game[:age_ratings]), # array of ids
+      release_date: Time.at(game[:first_release_date]).year, # Unix Time
+      cover:        "https:#{cover_url(game[:cover])}",
+      popularity:   game[:popularity], # good
+      summary:      game[:summary], # good
+      name:         game[:name], # good
+      total_rating: game[:total_rating], # good
+      categories:   game[:category], # got 0
+      genres:       run_array(:get_game_genres, :name, game[:genres]), # array of ids
+      keywords:     game[:keywords], # array of ids -- several hundred :(
+      platforms:    run_array(:get_platforms, :name, game[:platforms]), # array of ids
+      similars:     run_array(:get_games_by_id, :name, game[:similar_games]), # array of ids
+      screenshots:  format_screenshots(game), # array of ids
+      themes:       run_array(:get_game_themes, :name, game[:themes]), # array of ids
+      videos:       "https://www.youtube.com/watch?v=#{youtube_id(game[:videos])}" # good -> array of ids
+      }}
+    end
   end
     
 
